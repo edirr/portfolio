2b1a1cdffe83ede8f1a40686385e408300da70c7
Eric Dirr <edirr92@aol.com>
2019-01-09 11:18:57 -0500

    Updates

---
+++ precache-manifest.018b31b35de397e1c1dd552b6aeef4fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a841491be9c07b2edc6",
+    "url": "/static/css/main.9c1c8e5c.chunk.css"
+  },
+  {
+    "revision": "8a841491be9c07b2edc6",
+    "url": "/static/js/main.8a841491.chunk.js"
+  },
+  {
+    "revision": "13a1f5f66d31548c62fa",
+    "url": "/static/js/1.13a1f5f6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4c53b85189e213c291c3b36b0a2a90b2",
+    "url": "/static/media/Data-tracker-screenshot1.4c53b851.png"
+  },
+  {
+    "revision": "3e592e2c5f47a857a05c0f3676b4ac09",
+    "url": "/static/media/defender-dodge.3e592e2c.png"
+  },
+  {
+    "revision": "4c9cacf3c5459945ae8b72384c9fd53f",
+    "url": "/static/media/Screen Shot 2018-12-11 at 2.38.31 PM.4c9cacf3.png"
+  },
+  {
+    "revision": "c91314a395f25d845290daa05dd5066d",
+    "url": "/index.html"
+  }
+];
